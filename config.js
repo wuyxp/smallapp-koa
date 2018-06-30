43bdc6861dd057dee57aeb47b9aff00aa79cae44
wuyang <wuyang@www-macbook-pro.local>
2018-06-30 22:37:47 +0800

    修改的至少能跑起来了

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'small_app',
+    USERNAME: 'root',
+    PASSWORD: 'root',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
---
+++ static/build/webpack.base.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const sourcePath = path.join(__dirname, './static/src');
+const outputPath = path.join(__dirname, './../output/dist/');
+
+module.exports = {
+  
+  entry: {
+    'admin' : './static/src/pages/admin.js',
+    'work' : './static/src/pages/work.js',
+    'index' : './static/src/pages/index.js',
+    'error' : './static/src/pages/error.js',
+    vendor: ['react', 'react-dom', 'whatwg-fetch'],
+  },
+  output: {
+    path: outputPath,
+    publicPath: '/static/output/dist/',
+    filename: 'js/[name].js',
+  },
+  module: {
+
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'stage-0', 'react'],
+              plugins: [
+                "transform-runtime",
+                ["import", { libraryName: "antd", style: "css" }] // `style: true` 会加载 less 文件
+              ]
+              // cacheDirectory: true
+            },
+            
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader?modules'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'less-loader']
+        })
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      sourcePath,
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor'],
+      minChunks: Infinity,
+      filename: 'js/[name].js'
+    }),
+  ]
+};
