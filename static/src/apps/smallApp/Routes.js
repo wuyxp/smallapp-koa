4d29476f4e747b20c068d1fc2d928724c2bc8449
武扬 <956826374@qq.com>
2018-09-09 20:23:47 +0800

    配置好分片选项

--- static/src/apps/smallApp/Routes.js
+++ static/src/apps/smallApp/Routes.js
@@ -3,18 +3,27 @@ import {Router, Route,IndexRoute, browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 import store from './Store'
 
-import Home from './view/Home'
-import About from './view/About'
-import NotFound from './view/NotFound'
+// import Home from './view/Home'
+// import About from './view/About'
+// import NotFound from './view/NotFound'
 
 const history = syncHistoryWithStore(browserHistory, store);
+const getHomePage = (location, callback) => {
+  require.ensure([], require => callback(null, require('./view/Home').default), 'home')
+}
+const getAboutPage = (location, callback) => {
+  require.ensure([], require => callback(null, require('./view/About').default), 'about')
+}
+const getNotFoundPage = (location, callback) => {
+  require.ensure([], require => callback(null, require('./view/NotFound').default), '404')
+}
 const Routers = (props) => (
   <Router history={history}>
     <Route path={'/app/smallapp/'} component={props.component} >
-      <IndexRoute component={Home}/>
-      <Route path={`home`}  component={Home}/>
-      <Route path={`about`}  component={About}/>
-      <Route path={`*`} component={NotFound}/>
+      <IndexRoute getComponent={getHomePage}/>
+      <Route path={`home`}  getComponent={getHomePage}/>
+      <Route path={`about`}  getComponent={getAboutPage}/>
+      <Route path={`*`} getComponent={getNotFoundPage}/>
     </Route>
   </Router>
 )
